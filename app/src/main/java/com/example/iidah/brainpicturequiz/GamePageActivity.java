faae7afa64c4a5b30cfc2b2c7a75a831168aa2ab
igbinosa idahosa <idahosai@sheridancollege.ca>
2018-01-02 15:55:15 -0500

    made an intent to a new activity

---
+++ app/src/main/java/com/example/iidah/brainpicturequiz/GamePageActivity.java
@@ -0,0 +1,13 @@
+package com.example.iidah.brainpicturequiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GamePageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_page);
+    }
+}
